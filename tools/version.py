a71537f84d7b4d2d3083b6a94967d9d469c0b3d4
Devamitta <devamitta@sasanarakkha.org>
2025-04-20 12:34:44 +0800

    workflow: update version #15

--- tools/version.py
+++ tools/version.py
@@ -25,11 +25,13 @@ def make_version():
 
     version = f"v{major}.{minor}.{patch}"
     version_light = f"v{major}.{minor}"
+    version_ru = f"ru_v{major}.{minor}.{patch}"
 
     printer("version", version)
     printer("version light", version_light)
+    printer("version ru", version_ru)
 
-    return version, version_light
+    return version, version_light, version_ru
 
 
 def update_project_version(pth, version_light_light):
@@ -46,7 +48,7 @@ def update_project_version(pth, version_light_light):
 
 def update_db_version(pth, version):
     db_session = get_db_session(pth.dpd_db_path)
-    db_info = db_session.query(DbInfo).filter_by(key="dpd_release_version").first()
+    db_info = db_session.query(DbInfo).filter_by(key="dpd_sbs_release_version").first()
 
     # update the dpd_release_version if it exists
     if db_info:
@@ -54,29 +56,29 @@ def update_db_version(pth, version):
 
     # add the dpd_release_version and author info if it doesn't exist
     else:
-        dpd_release_version = DbInfo(key="dpd_release_version", value=version)
+        dpd_release_version = DbInfo(key="dpd_sbs_release_version", value=version)
         db_session.add(dpd_release_version)
 
         author = DbInfo(key="author", value="Bodhirasa")
         db_session.add(author)
 
-        email_address = DbInfo(key="email", value="digitalpalidictionary@gmail.com")
+        email_address = DbInfo(key="email", value="devamitta@sasanarakkha.org")
         db_session.add(email_address)
 
-        website = DbInfo(key="website", value="https://www.dpdict.net/")
+        website = DbInfo(key="website", value="https://dict.dhamma.gift/ru")
         db_session.add(website)
 
-        docs = DbInfo(key="docs", value="https://digitalpalidictionary.github.io/")
+        docs = DbInfo(key="docs", value="https://devamitta.github.io/dpd.rus/")
         db_session.add(docs)
 
         github = DbInfo(
-            key="github", value="https://github.com/digitalpalidictionary/dpd-db"
+            key="github", value="https://github.com/sasanarakkha/dpd-db-sbs"
         )
         db_session.add(github)
 
         latest_release = DbInfo(
             key="latest_release",
-            value="https://github.com/digitalpalidictionary/dpd-db/releases",
+            value="https://github.com/sasanarakkha/dpd-db-sbs/releases",
         )
         db_session.add(latest_release)
 
@@ -94,13 +96,14 @@ def main():
     pr.tic()
     pr.title("updating dpd release version")
     pth = ProjectPaths()
-    version, version_light = make_version()
+    version, version_light, version_ru = make_version()
 
-    config_update("version", "version", version, silent=True)
+    config_update("version", "version", version_ru, silent=True)
     printer("config.ini", "ok")
 
     # update_project_version(pth, version_light)
-    update_db_version(pth, version)
+    # update_db_version(pth, version)
+    update_db_version(pth, version_ru)
 
     pr.toc()
 
