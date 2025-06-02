c5e120cf6ea06aba230845fc4eee7499c6ccf22d
Devamitta <devamitta@sasanarakkha.org>
2025-06-02 16:04:05 +0800

    corrections

--- tools/fast_api_utils.py
+++ tools/fast_api_utils.py
@@ -4,12 +4,12 @@ import webbrowser
 
 
 def start_dpd_server():
-    "uvicorn exporter.webapp.main:app --host 127.1.1.1 --port 8080 --reload --reload-dir exporter/webapp"
+    "uvicorn exporter.webapp.main_ru:app --host 127.0.0.1 --port 8080 --reload --reload-dir exporter/webapp"
     command = [
         "uvicorn",
-        "exporter.webapp.main:app",
+        "exporter.webapp.main_ru:app",
         "--host",
-        "127.1.1.1",
+        "127.0.0.1",
         "--port",
         "8080",
         "--reload",
@@ -20,14 +20,14 @@ def start_dpd_server():
 
 
 def request_dpd_server(q: str | int):
-    base_url = "http://127.1.1.1:8080/"
+    base_url = "http://127.0.0.1:8080/sbs/"
     search_params = {"q": q}
     url = f"{base_url}?{urllib.parse.urlencode(search_params)}"
     webbrowser.open(url)
 
 
 def request_bold_def_server(search_1: str, search_2: str, option: str):
-    base_url = "http://127.1.1.2:8080/search"
+    base_url = "http://127.0.0.2:8080/search"
     search_params = {"search_1": search_1, "search_2": search_2, "option": option}
     url = f"{base_url}?{urllib.parse.urlencode(search_params)}"
     webbrowser.open(url)
