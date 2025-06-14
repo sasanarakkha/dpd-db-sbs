c7496c2584daa0253c747785d13f2ea7b28f3832
Devamitta <devamitta@sasanarakkha.org>
2025-06-14 17:49:35 +0800

    corrections

--- scripts/change_in_db/add_row_to_table.py
+++ scripts/change_in_db/add_row_to_table.py
@@ -116,8 +116,8 @@ if __name__ == "__main__":
     table_name = "SBS"
     # table_name = "Russian"
     column_to_update = "sbs_patimokkha"
-    value_to_update = "vib"
-    id_to_add = "31318"
+    value_to_update = "pat"
+    id_to_add = "82054"
 
     console.print(f"[bold yellow]Adding new row to the table '{table_name}' for ID '{id_to_add}'...[/bold yellow]")
     
